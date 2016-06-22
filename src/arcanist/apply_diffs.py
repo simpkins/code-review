c99bb6d88b23f8c11a24adb208ff6394f41ec5ca
Adam Simpkins <simpkins@fb.com>
2016-06-22 16:52:32 -0700

    Delete some old, unused debugging code

--- src/arcanist/apply_diffs.py
+++ src/arcanist/apply_diffs.py
@@ -166,39 +166,3 @@ Differential Revision: {uri}
 
         raise Exception('cannot apply diff for unknown arcanist project %s' %
                         diff_project)
-
-
-def _dump_changes(diff):
-    #_dump_diff(diff)
-    for change in diff.changes:
-        print('-' * 60)
-
-        _dump_change(change)
-        continue
-
-        if change.old_path != change.current_path:
-            print('%s --> %s' % (change.old_path, change.current_path))
-        else:
-            print(change.current_path)
-        print('%d hunks:' % len(change.hunks))
-        for hunk in change.hunks:
-            # corpus contains a unified diff, with a very large amount of
-            # context.
-            for k in sorted(hunk.keys()):
-                v = hunk[k]
-                print('  %s: %s' % (k, v))
-        #print(diff.get_patch())
-        #print(diff.get_patch())
-    print('=' * 60)
-    return
-
-def _dump_diff(diff):
-    diff.all_params['changes'] = None
-    diff.all_params['properties']['arc:unit'] = None
-    diff.all_params['properties']['facebook:contbuild'] = None
-    diff.all_params['properties']['facebook:sc_async'] = None
-
-    pprint.pprint(vars(diff), indent=2)
-
-def _dump_change(change):
-    pprint.pprint(vars(change), indent=2)
